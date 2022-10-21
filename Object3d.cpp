a16fcdad2f53d304d3135e42c6ff8c4bb15c568b
RyutoOzawa <k021g1163@g.neec.ac.jp>
2022-10-21 14:40:12 +0900

    AL4_02_01完了

--- Object3d.cpp
+++ Object3d.cpp
@@ -401,7 +401,7 @@ void Object3d::CreateModel()
 	//ファイルストリーム
 	ifstream file;
 	//.objファイルを開く
-	file.open("Resources/triangle/triangle.obj");
+	file.open("Resources/triangle_tex/triangle_tex.obj");
 	//ファイルオープン失敗をチェック
 	if (file.fail()) {
 		assert(0);
@@ -409,7 +409,7 @@ void Object3d::CreateModel()
 
 	vector<XMFLOAT3> positions;	//頂点座標
 	vector<XMFLOAT3> normals;	//法線ベクトル
-	vector<XMFLOAT2> texcords;	//テクスチャUV
+	vector<XMFLOAT2> texcoords;	//テクスチャUV
 	//1行ずつ読み込む
 	string line;
 	while (getline(file, line)) {
@@ -431,9 +431,9 @@ void Object3d::CreateModel()
 			//座標データに追加
 			positions.emplace_back(position);
 			//頂点データに追加
-			VertexPosNormalUv vertex{};
+		/*	VertexPosNormalUv vertex{};
 			vertex.pos = position;
-			vertices.emplace_back(vertex);
+			vertices.emplace_back(vertex);*/
 		}
 		//先頭文字列がfならポリゴン(三角形)
 		if (key == "f") {
@@ -442,12 +442,43 @@ void Object3d::CreateModel()
 			while (getline(line_stream, index_string, ' ')) {
 				//頂点インデックス1個分の文字列をストリームに変換して解析しやすくなる
 				istringstream index_stream(index_string);
-				unsigned short indexPosition;
+				unsigned short indexPosition, indexNoraml, indexTexcoord;
 				index_stream >> indexPosition;
+				index_stream.seekg(1, ios_base::cur); //スラッシュを飛ばす
+				index_stream >> indexTexcoord;
+				index_stream.seekg(1, ios_base::cur); //スラッシュを飛ばす
+				index_stream >> indexNoraml;
+				//頂点データの追加
+				VertexPosNormalUv vertex{};
+				vertex.pos = positions[indexPosition - 1];
+				vertex.normal = normals[indexNoraml - 1];
+				vertex.uv = texcoords[indexTexcoord - 1];
+				vertices.emplace_back(vertex);
 				//頂点インデックスに追加
-				indices.emplace_back(indexPosition - 1);
+				indices.emplace_back((unsigned short)indices.size());
 			}
 			
+		}
+		//先頭文字列がvtならテクスチャ
+		if (key == "vt") {
+			//U,V成分読み込み
+			XMFLOAT2 texcoord{};
+			line_stream >> texcoord.x;
+			line_stream >> texcoord.y;
+			//V方向反転
+			texcoord.y = 1.0f - texcoord.y;
+			//テクスチャ座標データに追加
+			texcoords.emplace_back(texcoord);
+		}
+		//先頭文字列がvnなら法線ベクトル
+		if (key == "vn") {
+			//X,Y,Z成分読み込み
+			XMFLOAT3 normal{};
+			line_stream >> normal.x;
+			line_stream >> normal.y;
+			line_stream >> normal.z;
+			//法線ベクトルデータに追加
+			normals.emplace_back(normal);
 		}
 	}
 	//ファイルを閉じる
